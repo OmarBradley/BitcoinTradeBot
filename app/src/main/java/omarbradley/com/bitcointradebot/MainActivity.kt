557257adce03d420491fd7ea243ab3bdde3a4fdd
이재화 <cenacolo34@gmail.com>
2019-02-25 00:17:54 +0900

    fabric 베타 부착

--- app/src/main/java/omarbradley/com/bitcointradebot/MainActivity.kt
+++ app/src/main/java/omarbradley/com/bitcointradebot/MainActivity.kt
@@ -2,9 +2,9 @@ package omarbradley.com.bitcointradebot
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.crashlytics.android.Crashlytics
 
 class MainActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
