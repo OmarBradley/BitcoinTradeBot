69ad5c6d3bbba3fd96f94d93a59f4f91db240bb6
이재화 <cenacolo34@gmail.com>
2019-03-06 00:48:20 +0900

    null pointer 경고 메시지 제거를 위해 리턴 타입 명시

--- common/src/main/java/omarbradley/com/common/util/TimeUtil.kt
+++ common/src/main/java/omarbradley/com/common/util/TimeUtil.kt
@@ -11,5 +11,5 @@ val timeSecond = timeMillis / 1000
 /**
  * todo 나중에 Date 관련 메소드들이 많이 추가된다면 DateUtil 클래스로 나눠서 관리해라
  */
-fun String.toDateTime(dateTimeFormatter: DateTimeFormatter = ISODateTimeFormat.dateTime()) =
+fun String.toDateTime(dateTimeFormatter: DateTimeFormatter = ISODateTimeFormat.dateTime()): DateTime =
     DateTime.parse(this, dateTimeFormatter)
